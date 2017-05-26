746b7993ab4864482567ae72f60b52e7eabd6809
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2017-05-26 22:26:28 +0300

    [TASK] Add languages to realurl config

--- project/web/typo3conf/realurl_conf.php
+++ project/web/typo3conf/realurl_conf.php
@@ -1,6 +1,15 @@
 <?php
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'] = array(
     '_DEFAULT' => array(
+        'preVars' => array(
+            array(
+                'GETvar' => 'L',
+                'valueMap' => array(
+                    'en' => 0,
+                    'ru' => 1,
+                ),
+            ),
+        ),
         'pagePath' => array(
             'rootpage_id' => 1,
         ),
